804b4d716069fea6ed4a45dbe71abd54f638854d
awab mahmod <awabmahmod408@gmail.com>
2026-01-22 18:02:16 +0200

    Stop tracking SQL files (now ignored)

--- add_unbillable_column.sql
+++
@@ -1,7 +0,0 @@
--- Add unbillable_days column to requests table
--- Run this in Supabase SQL Editor
-
-ALTER TABLE requests ADD COLUMN IF NOT EXISTS unbillable_days INTEGER DEFAULT 0;
-
--- Add comment
-COMMENT ON COLUMN requests.unbillable_days IS 'Number of vacation days that exceed the user available balance at the time of request';
